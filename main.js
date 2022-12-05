573f2aca63f55ceb3c99d5e864e0396fa613ffae
Pulkit Agrawal <48528949+Pulkit426@users.noreply.github.com>
2022-12-06 00:04:12 +0530

    add new book and display books logic

---
+++ main.js
@@ -0,0 +1,49 @@
+let myLibrary = []
+
+function Book(title, author, pages, readStatus){
+this.title = title
+this.author = author
+this.pages = pages
+this.readStatus = readStatus || "Not Read"
+}
+
+function addBookToLibrary(book){
+    myLibrary.push(book)
+}
+
+var book1  = new Book("Game of Thrones","George Martin",1000, "Read")
+var book2 = new Book("Harry Potter","JK Rowling",350)
+var book3 = new Book("1984","George Orwell", 400, "Not Read")
+var book4 = new Book("Atomic Habits","James Clear",250, "Read")
+addBookToLibrary(book1)
+addBookToLibrary(book2)
+addBookToLibrary(book3)
+addBookToLibrary(book4)
+
+var bookDisplayContainer = document.querySelector(".book-display")
+
+function displayBook(book){
+    let bookPara = document.createElement("p")
+    bookPara.innerText = ` ${book.title} by ${book.author} having ${book.pages} pages, ${book.readStatus}`
+    bookDisplayContainer.appendChild(bookPara)
+}
+
+function displayEveryBook(){
+    myLibrary.forEach( (book) => displayBook(book)  )
+}
+displayEveryBook()
+
+
+
+var formData = document.getElementsByTagName('input')
+
+var submitButton = document.querySelector('.submit-button')
+
+submitButton.addEventListener('click', (e) => {
+    e.preventDefault()
+    let bookValues = Array.from(formData).map( (item) =>  item.value) 
+    var newBook =  new Book(...bookValues)
+    addBookToLibrary(newBook)
+    displayBook(newBook)
+})
+
